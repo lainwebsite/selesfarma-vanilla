ecc38dd4d1ad4898485b3393322cf150b11c6b28
Neilson Soeratman <neilsonsoeratman@gmail.com>
2023-09-21 17:06:08 +0700

    contact form, products page hidden, footer logo cut fixed

---
+++ assets/js/contact-form.js
@@ -0,0 +1,22 @@
+$(document).on("submit", "#Contact-Form", function(e){
+    e.preventDefault();
+    var sourcemail = "info@selesfarma.com";
+    var link = "";
+
+    var name = $("#input-name").val();
+    var email = $("#input-email").val();
+    var subject = $("#input-email-subject").val();
+    var message = $("#input-message").val();
+
+    if (name != "" && subject != "" && email != "" && message != ""){
+        let isMobile = window.matchMedia("only screen and (max-width: 767px)").matches;
+        if (isMobile){
+            link = "mailto:" + sourcemail + "?subject=" + subject + "&body=" + name + "%0D%0A" + email + "%0D%0A%0D%0A" + message;
+            window.open(link, '_blank'); 
+        } else {
+            link = "https://mail.google.com/mail/?view=cm&fs=1&to=" + sourcemail + "&su=" + subject + "&body=" + name + "%0D%0A" + email + "%0D%0A%0D%0A" + message;
+            $("#form-submitter").attr("href", link);
+            $("#form-submitter")[0].click();
+        }
+    }    
+});
